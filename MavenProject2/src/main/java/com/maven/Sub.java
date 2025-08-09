9e56e71a12d4f233e2f643e8893c2116454ada08
tprak <tprak@LAPTOP-4ETB4P1M>
2025-08-09 18:12:47 +0530

    i add one method just normal method

--- MavenProject2/src/main/java/com/maven/Sub.java
+++ MavenProject2/src/main/java/com/maven/Sub.java
@@ -4,10 +4,16 @@ public class Sub {
     public void su()
     {
     	int a=50;
-    	int b=7;
+    	int b=78;
     	int c=a-b;
     	System.out.println(c);
-    }public static void main(String[] args) {
+    }
+    public void m3() {
+    	System.out.println("its a normal method");
+    }
+    
+    
+    public static void main(String[] args) {
 		Sub d=new Sub();
 		d.su();
 	}
